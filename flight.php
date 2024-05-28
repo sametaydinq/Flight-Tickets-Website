1852fe73d3cc8445e9e1652ccf6a334b280b657b
sametaydinq <sametaydinq@gmail.com>
2024-05-28 12:16:11 +0300

    add filter flight page

--- flight.php
+++ flight.php
@@ -1,3 +1,13 @@
+
+<?php
+
+
+include('db_connect.php');
+global $conn;
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,6 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles.css" />
     <title>Flight Ticket</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
     <nav>
@@ -18,56 +29,88 @@
     </nav>
   
    
-    <section class="section__container booking__container">
-        <div class="booking__nav">
-          <span>Economy Class</span>
-          <span>Business Class</span>
-          <span>First Class</span>
+   
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <h4>How to make Search box & filter data in HTML Table from Database in PHP MySQL </h4>
+                    </div>
+                    <div class="card-body text-align: center">
+                        <div class="row">
+                            <div class="col-md-7">
+
+                                <form action="" method="GET">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="search" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Search data">
+                                        <button type="submit" class="btn btn-primary">Search</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-body d-flex justify-content-center">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>From</th>
+                                    <th>Too</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                  
+                                    if(isset($_GET['search']))
+                                    {
+                                        $filtervalues = $_GET['search'];
+                                        $query = "SELECT * FROM ucus_listesi WHERE Fromm = '$filtervalues'";
+                                        $query_run = mysqli_query($conn, $query);
+
+                                        if(mysqli_num_rows($query_run) > 0)
+                                        {
+                                            foreach($query_run as $items)
+                                            {
+                                                ?>
+                                                <tr>
+                                                    <td><?= $items['Fromm']; ?></td>
+                                                    <td><?= $items['Too']; ?></td>
+                                                    <td><?= $items['Flight_Date']; ?></td>
+                                                    <td><?= $items['Flight_Time']; ?></td>
+                                                    <td><?= $items['Price']; ?></td>
+                                                </tr>
+                                                <?php
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                                <tr>
+                                                    <td colspan="4">No Record Found</td>
+                                                </tr>
+                                            <?php
+                                        }
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-        <form>
-          <div class="form__group">
-            <span><i class="ri-map-pin-line"></i></span>
-            <div class="input__content">
-              <div class="input__group">
-                <input type="text" />
-                <label>Location</label>
-              </div>
-              <p>Where are you goung?</p>
-            </div>
-          </div>
-          <div class="form__group">
-            <span><i class="ri-user-3-line"></i></span>
-            <div class="input__content">
-              <div class="input__group">
-                <input type="number" />
-                <label>Travellers</label>
-              </div>
-              <p>Add guests</p>
-            </div>
-          </div>
-          <div class="form__group">
-            <span><i class="ri-calendar-line"></i></span>
-            <div class="input__content">
-              <div class="input__group">
-                <input type="text" />
-                <label>Departure</label>
-              </div>
-              <p>Add date</p>
-            </div>
-          </div>
-          <div class="form__group">
-            <span><i class="ri-calendar-line"></i></span>
-            <div class="input__content">
-              <div class="input__group">
-                <input type="text" />
-                <label>Return</label>
-              </div>
-              <p>Add date</p>
-            </div>
-          </div>
-          <button class="btn"><i class="ri-search-line"></i></button>
-        </form>
-      </section>
+    </div>
+
+
 
       <section class="section__container travellers__container">
         <h2 class="section__header">Best travellers of the month</h2>
@@ -146,5 +189,9 @@
         <p>Copyright © 2023 FS Company. All rights reserved.</p>
         </div>
     </footer>
+
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
